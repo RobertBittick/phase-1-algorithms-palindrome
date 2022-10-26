5f2b019f1b3dd91ca26604b5ea9d25d8595f7b6a
Robert Bittick <robertbittick@gmail.com>
2022-10-26 16:44:54 -0700

    commit tree a13900b09eee0b298afc4b56c7184a1a47a97e4b

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
-}
+  function isPalindrome(string){
+    const arrayValues = string.split('')
+    const reverseArrayValues = arrayValues.reverse()
+    const reverseString = reverseArrayValues.join('')
+    if (string === reverseString) {
+      return true
+    } else {
+      return false
+    }
+  }
+isPalindrome('robot')
 
 /* 
   Add your pseudocode here
+
+  function takes a string and checks if it is a palindrome
+  if it is, return true
+  if it is not, return false
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
